4bbe9ee6d09a225809ee0eacef8ef7a8ccaeea82
TomiGie <paiguchi@gmail.com>
2016-01-20 15:22:20 +0900

    update grade file and change deprecated code

--- app/src/main/java/org/tohokutechdojo/changepicture/MainActivity.java
+++ app/src/main/java/org/tohokutechdojo/changepicture/MainActivity.java
@@ -1,12 +1,12 @@
 package org.tohokutechdojo.changepicture;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
     private int touchCount = 0;
@@ -18,23 +18,23 @@ public class MainActivity extends ActionBarActivity {
         // レイアウトファイル(activity_main.xml)を読み込む
         setContentView(R.layout.activity_main);
 
-        // 画像を入れるレイアウトを指定する
+        // 画像を入れるレイアウトを指定
         imageView = (ImageView) findViewById(R.id.imageView);
 
-        // 初期値の画像を入れる
+        // 初期値の画像を設定
         imageView.setImageResource(R.drawable.sheep_1);
     }
 
     public void onTouchLayout(View v) {
         if (touchCount == 0){
-            // 1種類目の画像を入れる
+            // 1種類目の画像を設定
             imageView.setImageResource(R.drawable.sheep_2);
 
             // touchCountを1にする
             touchCount = 1;
         }
         else {
-            // 2種類目の画像を入れる
+            // 2種類目の画像を設定
             imageView.setImageResource(R.drawable.sheep_1);
 
             // touchCountを0にする
